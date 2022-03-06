ab8f22d64ed73595194af0ae0183e3041f523c90
Ryohei Aiba <85457617+ryohei-aiba@users.noreply.github.com>
2022-03-06 16:35:44 +0900

    Bad example

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import axios from "axios";
+import { useState } from "react";
+import { Todo } from "./Todo";
 import "./styles.css";
 
 export default function App() {
+  const [todos, setTodos] = useState<any>([]);
+
+  const onClickFetchData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      setTodos(res.data);
+    });
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo title={todo.title} userid={todo.userId} />
+      ))}
     </div>
   );
 }
